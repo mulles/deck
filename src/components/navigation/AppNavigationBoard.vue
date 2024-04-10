b0e8d6f569637b221af0242eeb5667e67f3370a8
Luka Trovic <luka@nextcloud.com>
2024-04-10 20:02:33 +0200

    fix: permission check for cloning board
    Signed-off-by: Luka Trovic <luka@nextcloud.com>
    
    [skip ci]

--- src/components/navigation/AppNavigationBoard.vue
+++ src/components/navigation/AppNavigationBoard.vue
@@ -47,7 +47,7 @@
 					@click="actionEdit">
 					{{ t('deck', 'Edit board') }}
 				</NcActionButton>
-				<NcActionButton v-if="canManage && !board.archived"
+				<NcActionButton v-if="canCreate && !board.archived"
 					:close-after-click="true"
 					@click="actionClone">
 					<template #icon>
@@ -184,6 +184,7 @@ export default {
 			editColor: '',
 			isDueSubmenuActive: false,
 			updateDueSetting: null,
+			canCreate: canCreateState,
 		}
 	},
 	computed: {
@@ -252,6 +253,9 @@ export default {
 			try {
 				const newBoard = await this.$store.dispatch('cloneBoard', this.board)
 				this.loading = false
+				if (newBoard instanceof Error) {
+					throw newBoard
+				}
 				this.$router.push({ name: 'board', params: { id: newBoard.id } })
 			} catch (e) {
 				OC.Notification.showTemporary(t('deck', 'An error occurred'))
